783caf2699cd8d90f64445984b3a8686a3c906d5
Vardan Rastogi <vardanrastogi1@gmail.com>
2026-02-02 19:21:19 +0530

    Containerized using Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: process.cwd(),
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
